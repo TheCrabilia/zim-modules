b83275261ca9199dd47d3992af69f0ac3b7efaeb
TheCrabilia <69972734+TheCrabilia@users.noreply.github.com>
2023-05-29 22:44:35 +0300

    Don't save init script to file

--- zoxide/init.zsh
+++ zoxide/init.zsh
@@ -1,10 +1,3 @@
 (( ${+commands[zoxide]} )) && () {
-    local cmd=${commands[zoxide]}
-
-    local initfile=$1/zoxide.zsh
-    if [[ ! -e $initfile || $initfile -ot $cmd ]]; then
-        $cmd init --cmd cd zsh >| $initfile
-        print -u2 -PR "* Detected new version 'zoxide'. Regenerated init script."
-    fi
-    source $initfile
+    eval "$(${commands[zoxide]} init --cmd cd --hook pwd zsh)"
 } ${0:h}
